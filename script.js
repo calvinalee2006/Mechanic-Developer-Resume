83a5610eb3ddb8ececb56157d31d6db5bba35c49
Calvin Lee <Anthony2006Lee@hotmail.com>
2021-11-20 09:22:42 -0600

    finished about me page

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
-var slideIndex = 0;
-showSlides();
+//About me page, picture carousel
 
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 2000); }
+var i = 0;
+var images = [];
+var time = 3000;
+
+images[0] = './images/deadpool.jpg';
+images[1] = './images/tool_box.jpg';
+images[2] = './images/1594264565532.jpg';
+images[3] = './images/rg-31.jpg';
+images[4] = './images/profile.jpg';
+
+function changeImg() {
+    document.slide.src = images[i];
+    if(i < images.length- 1){
+        i++;
+    } else{
+        i = 0
+    }
+
+    setTimeout("changeImg()", time)
+}
+
+window.onload= changeImg;
+
